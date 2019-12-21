4e5fa931eced11bd9d8bee6045e947c82f1e8c65
Ken Lee <ken.lee821128@gmail.com>
2019-12-21 13:58:00 +0800

    java in

---
+++ ArrayTest.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ArrayTest {
+	private static Scanner sc;
+
+	public static void main(String[] args) {
+		sc = new Scanner(System.in);
+		int sum = 0;
+		int[] array = new int[4];
+		for(int i=0;i<4;i++) {
+			int input = sc.nextInt();
+			array[i] = input;
+			System.out.print(array[i]+" ");
+		}
+		System.out.println("");
+		for(int j=0;j<4;j++) {
+			sum += array[j];
+		}
+		System.out.println(sum);
+	}
+}
